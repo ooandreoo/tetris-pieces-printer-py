2415c7c058092b293ead510f6d4f19e18b24079d
ooandreoo <andre_av_5@outlook.com>
2025-10-15 10:17:00 -0300

    now rotation method in rotation_utils handles rotations with values different than 90, but only greater values and we dont validate that its possible to divide by 90

--- tools/rotation_utils.py
+++ tools/rotation_utils.py
@@ -1,7 +1,6 @@
 def rotate_positions(positions, center, degrees=90):
     states = [(0,1),(1,0),(0,-1),(-1,0)]
     rotations = degrees/90
-    state_changes = rotations % 4
 
     new_positions = []
     for position in positions:
@@ -11,21 +10,35 @@ def rotate_positions(positions, center, degrees=90):
         new_x = position[0]
         new_y = position[1]
 
-        # conditions help identify the current state
-        if(x_component > 0):
-            #what is inside the condition should state what to do depending on the amount of rotations, at the moment is 90 degrees rotation logic hardcoded
-            new_y -= x_component
-            new_x -= x_component
-        elif(x_component < 0):
-            new_y += abs(x_component)
-            new_x += abs(x_component)
+        if(x_component != 0):
 
-        if(y_component > 0):
-            new_x += y_component
-            new_y -= y_component
-        elif(y_component < 0):
-            new_x -= abs(y_component)
-            new_y += abs(y_component)
+            current_state_x = None
+
+            if(x_component > 0):
+                current_state_x = 1
+
+            elif(x_component < 0):
+                current_state_x = 3
+
+            final_state_x = int((current_state_x + rotations)%4)
+
+            new_y += (states[final_state_x][1]-states[current_state_x][1])*abs(x_component)
+            new_x += (states[final_state_x][0]-states[current_state_x][0])*abs(x_component)
+
+        if(y_component != 0):
+
+            current_state_y = None
+
+            if(y_component > 0):
+                current_state_y = 0
+            elif(y_component < 0):
+                current_state_y = 2
+
+
+            final_state_y = int((current_state_y + rotations)%4)
+
+            new_y += (states[final_state_y][1]-states[current_state_y][1])*abs(y_component)
+            new_x += (states[final_state_y][0]-states[current_state_y][0])*abs(y_component)
 
         new_positions.append((new_x,new_y))
 
